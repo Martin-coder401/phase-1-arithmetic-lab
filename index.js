f86b506e3aaf5c752bb8d5713994b7694ddb3caf
Martin Muthaura <mmuthaura 06@gmail.com>
2025-06-23 12:13:36 +0300

    Completed multiply, random, mod, and max assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+// Define num1 and num2 globally
+var num1 = 2;
+var num2 = 31;
+
+// Multiply
+var multiply = num1 * num2; // 62
+
+// Random (must be > 0)
+var random = Math.floor(Math.random() * 100) + 1;
+
+// Mod
+var num3 = 28;
+var num4 = 6;
+var mod = num3 % num4; // 4
+
+// Max
+var max = Math.max(5, 10, 15, 20); // 20
